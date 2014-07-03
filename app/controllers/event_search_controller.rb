a253279fd5277829c98d3610285b66861c6bbdb0
DaiFukumoto <wo.horn.dai@gmail.com>
2014-07-03 18:39:50 +0900

    検索キーワードを全角スペース，半角スペースで区切るとAND検索が出来るようにする.検索キーワードが入力されていない時はその月のイベントを全検索する．
    正規表現を用いて，全角スペースを半角スペースに置き換える処理を追加する．
    検索キーワードが入力されていない時は，その月のイベントを全検索する．

--- app/controllers/event_search_controller.rb
+++ app/controllers/event_search_controller.rb
@@ -3,17 +3,26 @@ require "date"
 
 class EventSearchController < ApplicationController
   def index
-    keyword = params[:event_search][:keyword]
-    p keyword
-
-    keywords = ["勉強会","AWS"]
-
+    if params[:event_search]
+      keywords = parseKeyword(params[:event_search][:keyword])
+    else
+      keywords = ""
+    end
     ### ATND APIからイベントを取得する.
     @events = getEvents(true, keywords)
 
   end
 
   private
+
+  ### 入力された文字列をスペースで区切って配列で返すメソッド
+  ### スペースは全角，半角どちらでもOK
+  ### 第1引数: 文字列
+  ### 返り値: 検索キーワードの配列
+  def parseKeyword(keyword)
+    keyword.gsub!(/[\s ]/," ")      # 全角スペースを半角スペースに置き換える
+    return keyword.to_s.split(nil)  # 半角スペースで区切って配列として返す
+  end
 
   ### 各種APIからkeywordを基にイベントを検索するプライベートメソッド
   ### 第1引数: AND,OR検索を判定する．
@@ -22,7 +31,13 @@ class EventSearchController < ApplicationController
   ### 返り値: イベント名，日付，場所，イベントURLを格納したハッシュを返す．
   def getEvents(condition, keyword)
     events = []
-    uri = generateQuery(condition, keyword)
+    if keyword.size > 0
+      ### 検索キーワードが入力されている時
+      uri = generateQuery_keyword(condition, keyword)
+    else
+      ### 検索キーワードが入力されていない時
+      uri = generateQuery()
+    end
     json = Net::HTTP.get(uri)
     result = JSON.parse(json)
     result['events'].each do |event|
@@ -36,7 +51,7 @@ class EventSearchController < ApplicationController
   ###         AND => true, OR => false
   ### 第2引数: keywordを格納した配列
   ### 返り値: ATND APIに対応したクエリを返す.
-  def generateQuery(condition, keywordArray)
+  def generateQuery_keyword(condition, keywordArray)
     ### 開催年月のパラメータを生成
     today =  Date.today.strftime("%Y%m")
     queryDate = 'ym=' + today
@@ -57,4 +72,21 @@ class EventSearchController < ApplicationController
     url = Addressable::URI.parse(url)
     return url
   end
+
+  ### クエリを生成するプライベートメソッド
+  ### 第1引数: AND,OR検索を判定する．
+  ###         AND => true, OR => false
+  ### 第2引数: keywordを格納した配列
+  ### 返り値: ATND APIに対応したクエリを返す.
+  def generateQuery()
+    ### 開催年月のパラメータを生成
+    today =  Date.today.strftime("%Y%m")
+    queryDate = 'ym=' + today
+    ### end
+    url = "http://api.atnd.org/events/?format=json"
+    url += "&#{queryDate}"
+    url = Addressable::URI.parse(url)
+    return url
+  end
+
 end
